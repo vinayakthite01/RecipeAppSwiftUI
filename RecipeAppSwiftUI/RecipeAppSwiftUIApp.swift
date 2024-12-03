809f74d7b2c1836811c875f06cb01fbb97b454a2
Vinayak Thite <vinayak@heady.io>
2024-12-03 15:01:21 +0530

    -Initial commit of project

---
+++ RecipeAppSwiftUI/RecipeAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppSwiftUIApp.swift
+//  RecipeAppSwiftUI
+//
+//  Created by Vinayak Thite on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
