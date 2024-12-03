2f5322a5d4ea65a68d765b30e58ffd1d60a60556
Vinayak Thite <vinayak@heady.io>
2024-12-03 17:34:38 +0530

    -Initial Commit for CategoryUI

---
+++ RecipeAppSwiftUI/Models/CategoriesModel.swift
@@ -0,0 +1,21 @@
+//
+//  CategoriesModel.swift
+//  RecipeAppSwiftUI
+//
+//  Created by Vinayak Thite on 03/12/24.
+//
+
+import Foundation
+
+struct CategoriesModel: Codable {
+    let categories: [Category]
+}
+
+struct Category: Codable, Identifiable {
+    let idCategory: String
+    let strCategory: String
+    let strCategoryThumb: String
+    let strCategoryDescription: String
+    
+    var id: String { idCategory }
+}
